43aa276239c4c72e78e6805b165c6e8e5a0480c2
vieirads <dennersvieira@gmail.com>
2022-06-30 06:22:47 -0300

    Translations on the scatter plot are finished. Moving to the pie chart.

--- src/components/ScatterPlot.jsx
+++ src/components/ScatterPlot.jsx
@@ -5,87 +5,94 @@
 import { ResponsiveLine } from "@nivo/line";
 import { FormattedMessage } from "react-intl";
 
-const ScatterPlot = ({ data }) => (
-  <ResponsiveLine
-    data={data}
-    margin={{ top: 20, right: 40, bottom: 60, left: 70 }}
-    xScale={{ type: "point" }}
-    yScale={{
-      type: "log",
-      min: "auto",
-      max: "auto",
-      stacked: false,
-      reverse: false,
-    }}
-    yFormat=" >-.0f"
-    axisTop={null}
-    axisRight={null}
-    axisBottom={{
-      orient: "bottom",
-      tickSize: 5,
-      tickPadding: 5,
-      tickRotation: 45,
-      legend: (
-        <FormattedMessage
-          id="dashboard.scatter.xlabel"
-          default="Tempo [anos]"
-        />
-      ),
-      legendOffset: 55,
-      legendPosition: "middle",
-    }}
-    axisLeft={{
-      orient: "left",
-      tickSize: 5,
-      tickPadding: 5,
-      tickRotation: 0,
-      legend: (
-        <FormattedMessage
-          id="dashboard.scatter.ylabel"
-          default="Ocorrências acumuladas"
-        />
-      ),
-      legendOffset: -65,
-      legendPosition: "middle",
-      tickValues: [
-        1, 10, 100, 1000, 10000, 100000, 1000000, 10000000, 100000000,
-      ],
-      format: ".1e",
-    }}
-    colors={["#e41a1d", "#367fb8", "#4daf4a"]}
-    pointSize={10}
-    pointColor={{ theme: "background" }}
-    pointBorderWidth={2}
-    pointBorderColor={{ from: "serieColor" }}
-    pointLabelYOffset={-12}
-    useMesh={true}
-    legends={[
-      {
-        anchor: "top-left",
-        direction: "column",
-        justify: false,
-        translateX: 10,
-        translateY: 0,
-        itemsSpacing: 0,
-        itemDirection: "left-to-right",
-        itemWidth: 80,
-        itemHeight: 20,
-        itemOpacity: 0.75,
-        symbolSize: 12,
-        symbolShape: "circle",
-        symbolBorderColor: "rgba(0, 0, 0, .5)",
-        effects: [
-          {
-            on: "hover",
-            style: {
-              itemBackground: "rgba(0, 0, 0, .03)",
-              itemOpacity: 1,
-            },
-          },
+import { Context } from "./LanguageWrapper";
+import { useContext } from "react";
+
+const ScatterPlot = ({ data }) => {
+  const context = useContext(Context);
+
+  return (
+    <ResponsiveLine
+      data={data[context.locale]}
+      margin={{ top: 20, right: 40, bottom: 60, left: 70 }}
+      xScale={{ type: "point" }}
+      yScale={{
+        type: "log",
+        min: "auto",
+        max: "auto",
+        stacked: false,
+        reverse: false,
+      }}
+      yFormat=" >-.0f"
+      axisTop={null}
+      axisRight={null}
+      axisBottom={{
+        orient: "bottom",
+        tickSize: 5,
+        tickPadding: 5,
+        tickRotation: 45,
+        legend: (
+          <FormattedMessage
+            id="dashboard.scatter.xlabel"
+            default="Tempo [anos]"
+          />
+        ),
+        legendOffset: 55,
+        legendPosition: "middle",
+      }}
+      axisLeft={{
+        orient: "left",
+        tickSize: 5,
+        tickPadding: 5,
+        tickRotation: 0,
+        legend: (
+          <FormattedMessage
+            id="dashboard.scatter.ylabel"
+            default="Ocorrências acumuladas"
+          />
+        ),
+        legendOffset: -65,
+        legendPosition: "middle",
+        tickValues: [
+          1, 10, 100, 1000, 10000, 100000, 1000000, 10000000, 100000000,
         ],
-      },
-    ]}
-  />
-);
+        format: ".1e",
+      }}
+      colors={["#e41a1d", "#367fb8", "#4daf4a"]}
+      pointSize={10}
+      pointColor={{ theme: "background" }}
+      pointBorderWidth={2}
+      pointBorderColor={{ from: "serieColor" }}
+      pointLabelYOffset={-12}
+      useMesh={true}
+      legends={[
+        {
+          anchor: "top-left",
+          direction: "column",
+          justify: false,
+          translateX: 10,
+          translateY: 0,
+          itemsSpacing: 0,
+          itemDirection: "left-to-right",
+          itemWidth: 80,
+          itemHeight: 20,
+          itemOpacity: 0.75,
+          symbolSize: 12,
+          symbolShape: "circle",
+          symbolBorderColor: "rgba(0, 0, 0, .5)",
+          effects: [
+            {
+              on: "hover",
+              style: {
+                itemBackground: "rgba(0, 0, 0, .03)",
+                itemOpacity: 1,
+              },
+            },
+          ],
+        },
+      ]}
+    />
+  );
+};
 
 export default ScatterPlot;
